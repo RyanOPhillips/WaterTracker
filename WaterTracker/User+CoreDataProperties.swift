d1b5b24c0e3be5132dc411a2aa2423143162482b
Ryan Phillips <rphillips4417@gmail.com>
2018-01-07 18:35:00 -0700

    Added another entity for core data tracking

--- WaterTracker/User+CoreDataProperties.swift
+++ WaterTracker/User+CoreDataProperties.swift
@@ -2,8 +2,9 @@
 //  User+CoreDataProperties.swift
 //  WaterTracker
 //
-//  Created by Ryan Phillips on 8/3/17.
+//  Created by Ryan Phillips on 11/9/17.
 //  Copyright © 2017 Ryan Phillips. All rights reserved.
+//
 //
 
 import Foundation
@@ -19,5 +20,6 @@ extension User {
     @NSManaged public var date: NSDate?
     @NSManaged public var units: Int16
     @NSManaged public var weight: Int16
+    @NSManaged public var amount: Int16
 
 }
